85d75f46736789b242fef8920338272087598b75
guikai <916126728@qq.com>
2019-05-06 23:23:40 +0800

    Movie Demo

---
+++ lib/mooc_base/MovieDemo.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(new MovieDemo());
+
+class MovieDemo extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
